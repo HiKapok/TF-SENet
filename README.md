93a281a9e495df6063400bf03dc16e478a930080
kapok <wangchangan@yeah.net>
2018-04-15 22:24:03 +0800

    add 101 model

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # SE\_ResNet and SE\_ResNeXt model for Tensorflow with pre-trained weights on ImageNet
 
-This repository contains code of the **un-official** re-implement of SE\_ResNe?t50 model. Here is the authors' [implementation](https://github.com/hujie-frank/SENet) in Caffe.
+This repository contains code of the **un-official** re-implement of SE\_ResNe?t50 and SE\_ResNe?t101 model. Here is the authors' [implementation](https://github.com/hujie-frank/SENet) in Caffe.
 
 SENet is one state-of-the-art convolutional neural network architecture, where dynamic channelwise
 feature recalibration have been introduced to improve the representational capacity of CNN. More details can be found in the original paper: [Squeeze-and-Excitation Networks](https://arxiv.org/pdf/1709.01507.pdf). 
 ##  ##
-In order to accelerate further research based on SE\_ResNet and SE\_ResNeXt, I would like to share the pre-trained weights on ImageNet for them, you can download from [Google Drive](https://drive.google.com/open?id=1k5MtfqbNRA8ziE3f18vu00Q1FQCzk4__). The pre-trained weights are converted from [official weights in Caffe](https://github.com/hujie-frank/SENet) using [MMdnn](https://github.com/Microsoft/MMdnn) with other post-processing. All rights related to the pre-trained weights belong to the original author of [SENet](https://github.com/hujie-frank/SENet).
+In order to accelerate further research based on SE\_ResNet and SE\_ResNeXt, I would like to share the pre-trained weights on ImageNet for them, you can download from [Google Drive](https://drive.google.com/open?id=1k5MtfqbNRA8ziE3f18vu00Q1FQCzk4__). The pre-trained weights are converted from [official weights in Caffe](https://github.com/hujie-frank/SENet) using [MMdnn](https://github.com/Microsoft/MMdnn) with other post-processing. And the outputs of network using the converted weights has almost the same outputs as original Caffe network (error<1e-5). All rights related to the pre-trained weights belong to the original author of [SENet](https://github.com/hujie-frank/SENet).
 
 **This code and the pre-trained weights only can be used for research purposes.**
 
-The canonical input image size for this SE\_ResNe?t50 is 224x224, each pixel value should in range [-128,128](BGR order), and the input preprocessing routine is quite simple, only normalization through mean channel subtraction was used. According to the official open-source  version in Caffe, these two models got to 22.37%(SE-ResNet-50) and 20.97%(SE-ResNeXt-50) on ImageNet-1k for the single crop top-1 validation error.
+The canonical input image size for this SE\_ResNe?t is 224x224, each pixel value should in range [-128,128](BGR order), and the input preprocessing routine is quite simple, only normalization through mean channel subtraction was used. According to the official open-source  version in Caffe, SE-ResNe?t models got to 22.37%(SE-ResNet-50) and 20.97%(SE-ResNeXt-50) on ImageNet-1k for the single crop top-1 validation error.
 
-The code is tested under Tensorflow 1.5, Python 3.5, Ubuntu 16.04. 
+The code is tested under Tensorflow 1.6, Python 3.5, Ubuntu 16.04. 
 
 BTW, other scaffold need to be build for training from scratch. You can refer to [resnet/imagenet_main](https://github.com/tensorflow/models/blob/22ded0410d5bed85a88329e852cd20882593652b/official/resnet/imagenet_main.py#L189) for adding weight decay to the loss manually.
 ##  ##
--- se_resnext.py
+++ se_resnext.py
@@ -14,6 +14,8 @@
 # ==============================================================================
 import tensorflow as tf
 
+import math
+
 USE_FUSED_BN = True
 BN_EPSILON = 9.999999747378752e-06
 BN_MOMENTUM = 0.99
@@ -21,14 +23,41 @@ BN_MOMENTUM = 0.99
 # input image order: BGR, range [0-255]
 # mean_value: 104, 117, 123
 # only subtract mean is used
+def constant_xavier_initializer(shape, group, dtype=tf.float32, uniform=True):
+    """Initializer function."""
+    if not dtype.is_floating:
+      raise TypeError('Cannot create initializer for non-floating point type.')
+    # Estimating fan_in and fan_out is not possible to do perfectly, but we try.
+    # This is the right thing for matrix multiply and convolutions.
+    if shape:
+      fan_in = float(shape[-2]) if len(shape) > 1 else float(shape[-1])
+      fan_out = float(shape[-1])/group
+    else:
+      fan_in = 1.0
+      fan_out = 1.0
+    for dim in shape[:-2]:
+      fan_in *= float(dim)
+      fan_out *= float(dim)
+
+    # Average number of inputs and output connections.
+    n = (fan_in + fan_out) / 2.0
+    if uniform:
+      # To get stddev = math.sqrt(factor / n) need to adjust for uniform.
+      limit = math.sqrt(3.0 * 1.0 / n)
+      return tf.random_uniform(shape, -limit, limit, dtype, seed=None)
+    else:
+      # To get stddev = math.sqrt(factor / n) need to adjust for truncated.
+      trunc_stddev = math.sqrt(1.3 * 1.0 / n)
+      return tf.truncated_normal(shape, 0.0, trunc_stddev, dtype, seed=None)
+
 # for root block, use dummy input_filters, e.g. 128 rather than 64 for the first block
-def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, data_format='channels_last', need_reduce=True, is_root=False, reduced_scale=16):
+def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, group, data_format='channels_last', need_reduce=True, is_root=False, reduced_scale=16):
     bn_axis = -1 if data_format == 'channels_last' else 1
     strides_to_use = 1
     residuals = inputs
     if need_reduce:
         strides_to_use = 1 if is_root else 2
-        proj_mapping = tf.layers.conv2d(inputs, input_filters * 2, (1, 1), use_bias=False,
+        proj_mapping = tf.layers.conv2d(inputs, input_filters, (1, 1), use_bias=False,
                                 name=name_prefix + '_1x1_proj', strides=(strides_to_use, strides_to_use),
                                 padding='valid', data_format=data_format, activation=None,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(),
@@ -36,8 +65,9 @@ def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, data_fo
         residuals = tf.layers.batch_normalization(proj_mapping, momentum=BN_MOMENTUM,
                                 name=name_prefix + '_1x1_proj/bn', axis=bn_axis,
                                 epsilon=BN_EPSILON, training=is_training, reuse=None, fused=USE_FUSED_BN)
-    reduced_inputs = tf.layers.conv2d(inputs, input_filters / 2, (1, 1), use_bias=False,
-                            name=name_prefix + '_1x1_reduce', strides=(strides_to_use, strides_to_use),
+
+    reduced_inputs = tf.layers.conv2d(inputs, input_filters // 2, (1, 1), use_bias=False,
+                            name=name_prefix + '_1x1_reduce', strides=(1, 1),
                             padding='valid', data_format=data_format, activation=None,
                             kernel_initializer=tf.contrib.layers.xavier_initializer(),
                             bias_initializer=tf.zeros_initializer())
@@ -46,18 +76,33 @@ def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, data_fo
                                         epsilon=BN_EPSILON, training=is_training, reuse=None, fused=USE_FUSED_BN)
     reduced_inputs_relu = tf.nn.relu(reduced_inputs_bn, name=name_prefix + '_1x1_reduce/relu')
 
+    if data_format == 'channels_first':
+        reduced_inputs_relu = tf.pad(reduced_inputs_relu, paddings = [[0, 0], [0, 0], [1, 1], [1, 1]])
+        weight_shape = [3, 3, reduced_inputs_relu.get_shape().as_list()[1]//group, input_filters // 2]
+        weight_ = tf.Variable(constant_xavier_initializer(weight_shape, group=group, dtype=tf.float32), trainable=is_training, name=name_prefix + '_3x3/kernel')
+        weight_groups = tf.split(weight_, num_or_size_splits=group, axis=-1, name=name_prefix + '_weight_split')
+        xs = tf.split(reduced_inputs_relu, num_or_size_splits=group, axis=1, name=name_prefix + '_inputs_split')
+    else:
+        reduced_inputs_relu = tf.pad(reduced_inputs_relu, paddings = [[0, 0], [1, 1], [1, 1], [0, 0]])
+        weight_shape = [3, 3, reduced_inputs_relu.get_shape().as_list()[-1]//group, input_filters // 2]
+        weight_ = tf.Variable(constant_xavier_initializer(weight_shape, group=group, dtype=tf.float32), trainable=is_training, name=name_prefix + '_3x3/kernel')
+        weight_groups = tf.split(weight_, num_or_size_splits=group, axis=-1, name=name_prefix + '_weight_split')
+        xs = tf.split(reduced_inputs_relu, num_or_size_splits=group, axis=-1, name=name_prefix + '_inputs_split')
 
-    conv3_inputs = tf.layers.conv2d(reduced_inputs_relu, input_filters / 2, (3, 3), use_bias=False,
-                                name=name_prefix + '_3x3', strides=(1, 1),
-                                padding='same', data_format=data_format, activation=None,
-                                kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                bias_initializer=tf.zeros_initializer())
+    convolved = [tf.nn.convolution(x, weight, padding='VALID', strides=[strides_to_use, strides_to_use], name=name_prefix + '_group_conv',
+                    data_format=('NCHW' if data_format == 'channels_first' else 'NHWC')) for (x, weight) in zip(xs, weight_groups)]
+
+    if data_format == 'channels_first':
+        conv3_inputs = tf.concat(convolved, axis=1, name=name_prefix + '_concat')
+    else:
+        conv3_inputs = tf.concat(convolved, axis=-1, name=name_prefix + '_concat')
+
     conv3_inputs_bn = tf.layers.batch_normalization(conv3_inputs, momentum=BN_MOMENTUM, name=name_prefix + '_3x3/bn',
                                         axis=bn_axis, epsilon=BN_EPSILON, training=is_training, reuse=None, fused=USE_FUSED_BN)
     conv3_inputs_relu = tf.nn.relu(conv3_inputs_bn, name=name_prefix + '_3x3/relu')
 
 
-    increase_inputs = tf.layers.conv2d(conv3_inputs_relu, input_filters * 2, (1, 1), use_bias=False,
+    increase_inputs = tf.layers.conv2d(conv3_inputs_relu, input_filters, (1, 1), use_bias=False,
                                 name=name_prefix + '_1x1_increase', strides=(1, 1),
                                 padding='valid', data_format=data_format, activation=None,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(),
@@ -71,15 +116,14 @@ def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, data_fo
     else:
         pooled_inputs = tf.reduce_mean(increase_inputs_bn, [1, 2], name=name_prefix + '_global_pool', keep_dims=True)
 
-    down_inputs = tf.layers.conv2d(pooled_inputs, (input_filters * 2) // reduced_scale, (1, 1), use_bias=True,
+    down_inputs = tf.layers.conv2d(pooled_inputs, input_filters // reduced_scale, (1, 1), use_bias=True,
                                 name=name_prefix + '_1x1_down', strides=(1, 1),
                                 padding='valid', data_format=data_format, activation=None,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(),
                                 bias_initializer=tf.zeros_initializer())
     down_inputs_relu = tf.nn.relu(down_inputs, name=name_prefix + '_1x1_down/relu')
 
-
-    up_inputs = tf.layers.conv2d(down_inputs_relu, input_filters * 2, (1, 1), use_bias=True,
+    up_inputs = tf.layers.conv2d(down_inputs_relu, input_filters, (1, 1), use_bias=True,
                                 name=name_prefix + '_1x1_up', strides=(1, 1),
                                 padding='valid', data_format=data_format, activation=None,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(),
@@ -91,9 +135,8 @@ def se_bottleneck_block(inputs, input_filters, name_prefix, is_training, data_fo
     return tf.nn.relu(pre_act, name=name_prefix + '/relu')
     #return tf.nn.relu(residuals + prob_outputs * increase_inputs_bn, name=name_prefix + '/relu')
 
-def SE_ResNet50(input_image, num_classes, is_training=False, data_format='channels_last'):
+def SE_ResNeXt50(input_image, num_classes, is_training = False, group=32, data_format='channels_last', net_depth=50):
     bn_axis = -1 if data_format == 'channels_last' else 1
-
     # the input image should in BGR order, note that this is not the common case in Tensorflow
     # convert from RGB to BGR
     if data_format == 'channels_last':
@@ -102,9 +145,13 @@ def SE_ResNet50(input_image, num_classes, is_training=False, data_format='channe
     else:
         image_channels = tf.unstack(input_image, axis=1)
         swaped_input_image = tf.stack([image_channels[2], image_channels[1], image_channels[0]], axis=1)
+    #swaped_input_image = input_image
 
-    input_depth = [128, 256, 512, 1024] # the input depth of the the first block is dummy input
-    num_units = [3, 4, 6, 3]
+    if net_depth not in [50, 101]:
+        raise TypeError('Only ResNeXt50 or ResNeXt101 is supprted now.')
+    input_depth = [256, 512, 1024, 2048] # the input depth of the the first block is dummy input
+    num_units = [3, 4, 6, 3] if net_depth==50 else [3, 4, 23, 3]
+
     block_name_prefix = ['conv2_{}', 'conv3_{}', 'conv4_{}', 'conv5_{}']
 
     if data_format == 'channels_first':
@@ -112,7 +159,7 @@ def SE_ResNet50(input_image, num_classes, is_training=False, data_format='channe
     else:
         swaped_input_image = tf.pad(swaped_input_image, paddings = [[0, 0], [3, 3], [3, 3], [0, 0]])
 
-    inputs_features = tf.layers.conv2d(swaped_input_image, input_depth[0]//2, (7, 7), use_bias=False,
+    inputs_features = tf.layers.conv2d(swaped_input_image, input_depth[0]//4, (7, 7), use_bias=False,
                                 name='conv1/7x7_s2', strides=(2, 2),
                                 padding='valid', data_format=data_format, activation=None,
                                 kernel_initializer=tf.contrib.layers.xavier_initializer(),
@@ -129,9 +176,9 @@ def SE_ResNet50(input_image, num_classes, is_training=False, data_format='channe
     for ind, num_unit in enumerate(num_units):
         need_reduce = True
         for unit_index in range(1, num_unit+1):
-            inputs_features = se_bottleneck_block(inputs_features, input_depth[ind], block_name_prefix[ind].format(unit_index), is_training=is_training, data_format=data_format, need_reduce=need_reduce, is_root=is_root)
+            inputs_features = se_bottleneck_block(inputs_features, input_depth[ind], block_name_prefix[ind].format(unit_index), is_training=is_training, group=group, data_format=data_format, need_reduce=need_reduce, is_root=is_root)
             need_reduce = False
-            is_root = False
+        is_root = False
 
     if data_format == 'channels_first':
         pooled_inputs = tf.reduce_mean(inputs_features, [2, 3], name='pool5/7x7_s1', keep_dims=True)
@@ -146,6 +193,7 @@ def SE_ResNet50(input_image, num_classes, is_training=False, data_format='channe
 
     return logits_output, tf.nn.softmax(logits_output, name='prob')
 
+
 '''run test for the chcekpoint again
 '''
 import numpy as np
@@ -153,7 +201,7 @@ import numpy as np
 tf.reset_default_graph()
 
 input_image = tf.placeholder(tf.float32,  shape = (None, 3, 224, 224), name = 'input_placeholder')
-outputs = SE_ResNet50(input_image, 1000, is_training = False, data_format='channels_first')
+outputs = SE_ResNeXt50(input_image, 1000, is_training = False, data_format='channels_first')
 
 saver = tf.train.Saver()
 
@@ -161,10 +209,9 @@ with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    saver.restore(sess, "./model/seresnet50/se_resnet50.ckpt")
+    saver.restore(sess, "./model/seresnext50/se_resnext50.ckpt")
 
     predict = sess.run(outputs, feed_dict = {input_image : np.expand_dims(np.concatenate([np.ones((1, 224,224))*0.2, np.ones((1, 224,224))*0.4, np.ones((1, 224,224))*0.6], axis=0), axis=0)})
     #predict = sess.run(outputs, feed_dict = {input_image : np.expand_dims(np.concatenate([np.ones((224,224,1))*0.2, np.ones((224,224,1))*0.4, np.ones((224,224,1))*0.6], axis=-1), axis=0)})
     print(predict)
     print(np.argmax(predict))
-
